02154de511b6a3e6075c3683660cb066e99a05af
Farid-Adwani <78969623+Farid-Adwani@users.noreply.github.com>
2021-05-23 22:43:29 +0100

    optimising code and adding and linking

--- src/Controller/ClubsController.php
+++ src/Controller/ClubsController.php
@@ -3,18 +3,52 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClubsController extends AbstractController
 {
+
+
+    /**
+    *@Route("/vide",name="vide")
+     */
+    function vide( ): Response{
+        return $this->render('clubs/vide.html.twig',[ 'title'=>"°VIDE°" ]);
+    }
+
+
+    /**
+     * @Route("/test/{fullname<^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$>?foulen}/{Age<\d+>?18}/{Section?mpi}",name="test")
+     */
+    function test( $fullname,$Age,$Section, SessionInterface $session): Response{
+        if(!$session->has('user')){
+            $this->addFlash('message',"Hello back Mr $fullname");
+            $session->set('user',$fullname);
+        }
+        dump($session);
+        return $this->render('arbtx/3.twig',[ 'fullname'=>$fullname, 'age'=>$Age,'section'=>$Section ]);
+    }
+
+
     #[Route('/', name: 'landing')]
     public function index(): Response
     {
-        return $this->render('clubs/index.html.twig', [
-            'controller_name' => 'ClubsController',
-        ]);
+        return $this->redirectToRoute('home');
     }
+
+    #[Route('/home', name: 'home')]
+    public function home(): Response {
+        return $this->render('clubs/index.html.twig', []);
+    }
+
+    #[Route('/account', name: 'account')]
+    public function account(): Response {
+        return $this->render('clubs/Account.html.twig', []);
+    }
+
     #[Route('/contact', name: 'contact')]
     public function contact(): Response {
         return $this->render('clubs/contact.html.twig', []);
@@ -40,9 +74,6 @@ class ClubsController extends AbstractController
     public function products(): Response {
         return $this->render('clubs/products.html.twig', []);
     }
-    #[Route('/home', name: 'home')]
-    public function home(): Response {
-        return $this->render('clubs/home.html.twig', []);
-    }
+
 
 }
